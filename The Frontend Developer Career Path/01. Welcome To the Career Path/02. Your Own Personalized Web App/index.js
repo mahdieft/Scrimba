ea9fe651f7832ee89bf94eb7872b205369169a9f
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-02-17 18:39:21 +0800

    The Frontend Developer Career Path - Welcome To the Career Path - Your Own Personalized Web App - Display Photos

--- The Frontend Developer Career Path/01. Welcome To the Career Path/02. Your Own Personalized Web App/index.js
+++ The Frontend Developer Career Path/01. Welcome To the Career Path/02. Your Own Personalized Web App/index.js
@@ -22,7 +22,7 @@ const App = (props) => {
 
     // CHALLENGE:
     // Change the query to one of your interests
-    let [query, setQuery] = useState('');
+    let [query, setQuery] = useState('nature');
     const queryInput = useRef(null);
 
     const numberOfPhotos = 20;
