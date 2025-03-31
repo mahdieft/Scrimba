7d8ca1893413f18b57f3e23d74e5134150c25fe5
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-03-31 11:54:28 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Passenger Counter App - The onclick event listener

--- The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
+++ The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
@@ -1,11 +1,3 @@
-let bonusPoints = 50;
-console.log(bonusPoints);
-
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
-
-bonusPoints = bonusPoints - 75;
-console.log(bonusPoints);
-
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+function increment() {
+    console.log('The button was clicked');
+}
