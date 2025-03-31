d1802ec8429d91f91e4aeb0ac092544e2552411d
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-03-31 19:34:24 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Passenger Counter App - Using functions to write less code

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/01. Using Functions To Write Less Code/index.js
@@ -0,0 +1,19 @@
+function countdown() {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+
+// Setting up the the race 🏎 🏎 🏎
+
+countdown();
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+countdown();
