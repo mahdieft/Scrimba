481d55334f891f0ee325fec555a782f8fce4946b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-03-31 11:33:29 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Passenger Counter App - Reassigning and incrementing

--- The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
+++ The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
@@ -1,5 +1,11 @@
-let myAge = 35;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
+let bonusPoints = 50;
+console.log(bonusPoints);
 
-console.log(myDogAge);
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
