319af2717f89f08f0fc451726978ec32171eff1b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-03-31 20:03:56 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Passenger Counter App - Display the count

--- The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
+++ The Frontend Developer Career Path/03. Making Websites Interactive/01. Build a Passenger Counter App/index.js
@@ -1,6 +1,10 @@
+let countEl = document.getElementById('count-el');
+
+console.log(countEl);
+
 let count = 0;
 
 function increment() {
     count = count + 1;
-    console.log(count);
+    countEl.innerText = count;
 }
