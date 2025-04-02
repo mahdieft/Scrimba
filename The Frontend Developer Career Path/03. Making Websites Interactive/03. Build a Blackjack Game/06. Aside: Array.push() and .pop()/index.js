c29df9d73c8cf22b5ca793979e22274b65d45b5c
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-02 13:21:31 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Blackjack Game - Aside: Array.push() and .pop()

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/03. Build a Blackjack Game/06. Aside: Array.push() and .pop()/index.js
@@ -0,0 +1,11 @@
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+console.log(messages)
+
+console.log(messages.pop())
