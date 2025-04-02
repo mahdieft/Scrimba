f4f0199e16dbb43998697524a259bf0bd5d683e7
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-02 11:40:18 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Blackjack Game - Make the start button work

--- The Frontend Developer Career Path/03. Making Websites Interactive/03. Build a Blackjack Game/index.js
+++ The Frontend Developer Career Path/03. Making Websites Interactive/03. Build a Blackjack Game/index.js
@@ -1,20 +1,19 @@
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = 10;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = true
-let message = ""
+let isAlive = true;
+let message = '';
 
-if (sum <= 20) {
-    message = 'Do you want to draw a new card? 🙂';
-} else if (sum === 21) {
-    message = 'Wohoo! You\'ve got Blackjack! 🥳';
-    hasBlackJack = true;
-} else {
-    message = 'You\'re out of the game! 😭';
+function startGame() {
+    if (sum <= 20) {
+        message = 'Do you want to draw a new card? 🙂';
+    } else if (sum === 21) {
+        message = 'Wohoo! You\'ve got Blackjack! 🥳';
+        hasBlackJack = true;
+    } else {
+        message = 'You\'re out of the game! 😭';
+        isAlive = false;
+    }
+    console.log(message);
 }
-
-// CASH OUT!
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message)
