96e979fca85c09a7aed4f0f97143a0b10bf44e7f
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-03 21:02:19 -0400

    The Frontend Developer Career Path - Making Websites Interactive - JavaScript Challenges - Part 2 - Loops and arrays

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/04. JavaScript Challenges - Part 2/03. Loops and Arrays/index.js
@@ -0,0 +1,15 @@
+let largeCountries = ['China', 'India', 'USA', 'Indonesia', 'Pakistan'];
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indonesia
+- Pakistan
+*/
+console.log('The 5 largest countries in the world:');
+for (let c of largeCountries) {
+    console.log('- ' + c);
+}
