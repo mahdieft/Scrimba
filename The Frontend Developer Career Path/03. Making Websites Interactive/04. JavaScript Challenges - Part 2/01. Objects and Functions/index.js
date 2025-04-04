928145bab34065e94fbb17fcf6ed37ac42d6be63
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-03 20:40:39 -0400

    The Frontend Developer Career Path - Making Websites Interactive - JavaScript Challenges - Part 2 - Objects and functions

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/04. JavaScript Challenges - Part 2/01. Objects and Functions/index.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Mahdi',
+    age: 46,
+    country: 'Canada',
+};
+
+function logData() {
+    console.log(person.name + ' is ' + person.age + ' years old and lives in ' + person.country);
+}
+
+logData();
