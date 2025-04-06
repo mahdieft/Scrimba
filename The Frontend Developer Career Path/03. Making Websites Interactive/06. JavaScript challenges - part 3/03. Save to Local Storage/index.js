d52588bf2e158e7c59bba92a9b32351bc59e778b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-06 07:17:26 -0400

    The Frontend Developer Career Path - Making Websites Interactive - JavaScript Challenges - Part 3 - save to localStorage

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/06. JavaScript challenges - part 3/03. Save to Local Storage/index.js
@@ -0,0 +1,5 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+localStorage.setItem('Name', 'Mahdi')
+console.log(localStorage.getItem('Name'))
