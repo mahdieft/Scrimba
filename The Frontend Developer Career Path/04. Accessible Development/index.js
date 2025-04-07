841ca2b7edaeff7855d5407651b31aa7dd59d0e6
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-07 08:31:24 -0400

    The Frontend Developer Career Path - Accessible Development - Aside: Accessible JavaScript

--- The Frontend Developer Career Path/04. Accessible Development/index.js
+++ The Frontend Developer Career Path/04. Accessible Development/index.js
@@ -1,10 +1,28 @@
-function sendMessage() {
-    // Get the button element
-    const button = document.getElementById('submitButton');
+function checkInputs() {
+    const name = document.getElementById('name').value;
+    const address = document.getElementById('address').value;
+    const city = document.getElementById('city').value;
+    const postcode = document.getElementById('postcode').value;
+    const email = document.getElementById('email').value;
+    const yesRadio = document.getElementById('yes').checked;
+    const noRadio = document.getElementById('no').checked;
 
-    // Replace the button with a paragraph
-    button.outerHTML = '<p id="submitMessage" class="submit-message" aria-live="polite">Message sent! ✅</p>';
+    // Checks if all fields are filled (truthy) and at least one radio button is selected
+    const allFilled = name && address && city && postcode && email && (yesRadio || noRadio);
 
-    // Get the home link element and add focus to it
-    document.getElementById('homeLink').focus();
+    if (allFilled) {
+        popoverMessage.style.opacity = '0';
+    }
+
+    document.getElementById('submitButton').disabled = !allFilled;
+    return allFilled;
 }
+
+// Event listeners for each input field
+document.getElementById('name').addEventListener('input', checkInputs);
+document.getElementById('address').addEventListener('input', checkInputs);
+document.getElementById('city').addEventListener('change', checkInputs);
+document.getElementById('postcode').addEventListener('input', checkInputs);
+document.getElementById('email').addEventListener('input', checkInputs);
+document.getElementById('yes').addEventListener('change', checkInputs);
+document.getElementById('no').addEventListener('change', checkInputs);
