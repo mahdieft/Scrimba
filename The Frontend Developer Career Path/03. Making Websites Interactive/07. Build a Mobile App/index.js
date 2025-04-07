14e9e3548add30f8bbc682233ef94d09240482a9
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-07 07:03:14 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Mobile App - Delete unneeded code

--- The Frontend Developer Career Path/03. Making Websites Interactive/07. Build a Mobile App/index.js
+++ The Frontend Developer Career Path/03. Making Websites Interactive/07. Build a Mobile App/index.js
@@ -2,26 +2,22 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.6.0/firebas
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js';
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-ap-default-rtdb.firebaseio.com/"
+    databaseURL: 'https://leads-tracker-ap-default-rtdb.firebaseio.com/',
 };
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
+const database = getDatabase(app);
 
-let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
 deleteBtn.addEventListener('dblclick', () => {
-    myLeads = [];
-    renderLeads(myLeads);
 });
 
 inputBtn.addEventListener('click', function () {
-    myLeads.push(inputEl.value);
+    console.log(inputEl.value);
     inputEl.value = '';
-    renderLeads(myLeads);
 });
 
 function renderLeads(leads) {
@@ -30,4 +26,4 @@ function renderLeads(leads) {
         list += `<li><a href="${lead}" target="_blank">${lead}</a></li>`;
     }
     ulEl.innerHTML = list;
-};
+}
