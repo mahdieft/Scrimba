f24ef46c1131a8a0e1f5483f90affde3dab56c7a
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-07 06:49:28 -0400

    The Frontend Developer Career Path - Making Websites Interactive - Build a Mobile App - Create new Firebase realtime database

---
+++ The Frontend Developer Career Path/03. Making Websites Interactive/07. Build a Mobile App/index.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js';
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-ap-default-rtdb.firebaseio.com/"
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
+const ulEl = document.getElementById('ul-el');
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads(myLeads);
+}
+
+deleteBtn.addEventListener('dblclick', () => {
+    localStorage.clear();
+    myLeads = [];
+    renderLeads(myLeads);
+});
+
+inputBtn.addEventListener('click', function () {
+    myLeads.push(inputEl.value);
+    inputEl.value = '';
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    renderLeads(myLeads);
+});
+
+tabBtn.addEventListener('click', function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        renderLeads(myLeads);
+    });
+});
+
+function renderLeads(leads) {
+    let list = '';
+    for (let lead of leads) {
+        list += `<li><a href="${lead}" target="_blank">${lead}</a></li>`;
+    }
+    ulEl.innerHTML = list;
+};
