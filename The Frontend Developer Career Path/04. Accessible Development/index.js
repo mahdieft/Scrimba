360daf9cacbfd7a2c1e7ba6310d8862a8da3d1e4
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-07 08:38:45 -0400

    The Frontend Developer Career Path - Accessible Development - Skip Navigation Link

--- The Frontend Developer Career Path/04. Accessible Development/index.js
+++ The Frontend Developer Career Path/04. Accessible Development/index.js
@@ -1,28 +1,7 @@
-function checkInputs() {
-    const name = document.getElementById('name').value;
-    const address = document.getElementById('address').value;
-    const city = document.getElementById('city').value;
-    const postcode = document.getElementById('postcode').value;
-    const email = document.getElementById('email').value;
-    const yesRadio = document.getElementById('yes').checked;
-    const noRadio = document.getElementById('no').checked;
+// Clearing out the input fields on clicking the submit button
+document.querySelector(".submit-button").onclick = function() {submitForm()};
 
-    // Checks if all fields are filled (truthy) and at least one radio button is selected
-    const allFilled = name && address && city && postcode && email && (yesRadio || noRadio);
-
-    if (allFilled) {
-        popoverMessage.style.opacity = '0';
-    }
-
-    document.getElementById('submitButton').disabled = !allFilled;
-    return allFilled;
+const submitForm = () => {
+    document.getElementById("name").value = ""
+    document.getElementById("email").value = ""
 }
-
-// Event listeners for each input field
-document.getElementById('name').addEventListener('input', checkInputs);
-document.getElementById('address').addEventListener('input', checkInputs);
-document.getElementById('city').addEventListener('change', checkInputs);
-document.getElementById('postcode').addEventListener('input', checkInputs);
-document.getElementById('email').addEventListener('input', checkInputs);
-document.getElementById('yes').addEventListener('change', checkInputs);
-document.getElementById('no').addEventListener('change', checkInputs);
