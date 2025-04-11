2ac95dc3488da73079e97aa38a9893aea6751ec5
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-11 16:08:11 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - The World's Most Annoying Cookie Consent - There is no choice 1

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/index.js
@@ -3,10 +3,15 @@ const modalInner = document.getElementById('modal-inner');
 const modalCloseBtn = document.getElementById('modal-close-btn');
 const consentForm = document.getElementById('consent-form');
 const modalText = document.getElementById('modal-text');
+const declineBtn = document.getElementById('decline-btn');
 
 setTimeout(function () {
     modal.style.display = 'inline';
 }, 1500);
+
+declineBtn.addEventListener('mouseenter', function () {
+    console.log('hovered');
+});
 
 modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none';
