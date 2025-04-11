bb1139f41956464c7a2763d38a8ed57a8d2a00b2
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-11 14:49:17 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - The World's Most Annoying Cookie Consent - Aside: FormData methods

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/03. Aside: Forms/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/03. Aside: Forms/index.js
@@ -4,5 +4,9 @@ loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const loginFormData = new FormData(loginForm);
-    console.log(loginFormData);
+    const name = loginFormData.get('astronautName');
+    const email = loginFormData.get('astronautEmail');
+    const password = loginFormData.get('astronautPassword');
+
+    console.log(name, email, password);
 });
