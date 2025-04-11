1dfd6935e9413ee5ef0f4a3332bb90f4e8a73754
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-11 15:00:52 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - The World's Most Annoying Cookie Consent - Form Data 2

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/01. The World's Most Annoying Cookie Consent/index.js
@@ -16,12 +16,12 @@ consentForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const consentFormData = new FormData(consentForm);
-    console.log(consentFormData);
+    const name = consentFormData.get('fullName');
 
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
+        <p id="upload-text">
             Uploading your data to the dark web...
         </p>
     </div>`;
@@ -31,12 +31,12 @@ consentForm.addEventListener('submit', (e) => {
     }, 1500);
 
     setTimeout(() => {
-        modalInner.innerHTML = `
-            <h2>Thanks you sucker! </h2>
-            <p>We just sold the rights to your eternal soul.</p>
-            <div class="idiot-gif">
-                <img src="images/pirate.gif">
-            </div>
-        `;
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `;
     }, 3000);
 });
