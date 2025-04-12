c9fdd7df606903f0f88be86ca9a0511978c09578
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-12 11:03:06 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Pumpkin's Purrfect Meme Picker - The order of things

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
@@ -8,12 +8,8 @@ const memeModal = document.getElementById('meme-modal');
 const memeModalCloseBtn = document.getElementById('meme-modal-close-btn');
 
 emotionRadios.addEventListener('change', highlightCheckedOption);
-getImageBtn.addEventListener('click', renderCat);
 memeModalCloseBtn.addEventListener('click', closeModal);
-
-function closeModal() {
-    memeModal.style.display = 'none';
-}
+getImageBtn.addEventListener('click', renderCat);
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio');
@@ -21,6 +17,33 @@ function highlightCheckedOption(e) {
         radio.classList.remove('highlight');
     }
     document.getElementById(e.target.id).parentElement.classList.add('highlight');
+}
+
+function closeModal() {
+    memeModal.style.display = 'none';
+}
+
+function renderCat() {
+    const catObject = getSingleCatObject();
+    memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `;
+    memeModal.style.display = 'flex';
+}
+
+function getSingleCatObject() {
+    const catsArray = getMatchingCatsArray();
+
+    if (catsArray.length === 1) {
+        return catsArray[0];
+    } else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length);
+        return catsArray[randomNumber];
+    }
 }
 
 function getMatchingCatsArray() {
@@ -38,27 +61,6 @@ function getMatchingCatsArray() {
         });
         return matchingCatsArray;
     }
-}
-
-function getSingleCatObject() {
-    const catsArray = getMatchingCatsArray();
-    if (catsArray.length === 1) {
-        return catsArray[0];
-    } else {
-        return catsArray[Math.floor(Math.random() * catsArray.length)];
-    }
-}
-
-function renderCat() {
-    const catObject = getSingleCatObject();
-    memeModalInner.innerHTML = `
-        <img 
-        class="cat-img" 
-        src="./images/${catObject.image}"
-        alt="${catObject.alt}"
-        >
-        `;
-    memeModal.style.display = 'flex';
 }
 
 function getEmotionsArray(cats) {
