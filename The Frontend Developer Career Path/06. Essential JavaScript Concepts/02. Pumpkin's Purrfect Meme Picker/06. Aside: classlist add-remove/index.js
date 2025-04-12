aee8b4b63e989f6fa919816d37fb7bc41df72e94
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-12 09:25:44 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Pumpkin's Purrfect Meme Picker - Aside: classlist add/remove

---
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/06. Aside: classlist add-remove/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('click', function (e) {
+    document.getElementById(e.target.id).parentElement.classList.add('read');
+    document.getElementById(e.target.id).parentElement.classList.remove('unread');
+});
