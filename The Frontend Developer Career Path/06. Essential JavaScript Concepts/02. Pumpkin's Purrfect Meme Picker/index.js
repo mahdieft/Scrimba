66fc1a1239a38101f2d89313e8b0ab21ea8a011e
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-12 08:37:03 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Pumpkin's Purrfect Meme Picker - Render out the emotions 2

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
@@ -169,6 +169,8 @@ const catsData = [
     },
 ];
 
+const emotionRadios = document.getElementById('emotion-radios');
+
 function getEmotionsArray(cats) {
     const emotionsArray = [];
     for (let cat of cats) {
@@ -181,7 +183,12 @@ function getEmotionsArray(cats) {
 }
 
 function renderEmotionsRadios(cats) {
+    let radioItems = ``;
     const emotions = getEmotionsArray(cats);
-    console.log(emotions);
+    for (let emotion of emotions) {
+        radioItems += `<p>${emotion}</p>`;
+    }
+    emotionRadios.innerHTML = radioItems;
 }
+
 renderEmotionsRadios(catsData);
