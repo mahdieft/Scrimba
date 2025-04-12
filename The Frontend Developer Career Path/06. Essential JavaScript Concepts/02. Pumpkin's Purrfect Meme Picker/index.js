853ec9c817d9545e6fe60e831a4efc07acc2e6ff
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-12 10:57:23 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Pumpkin's Purrfect Meme Picker - renderCat()

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/index.js
@@ -3,6 +3,8 @@ import { catsData } from './data.js';
 const emotionRadios = document.getElementById('emotion-radios');
 const getImageBtn = document.getElementById('get-image-btn');
 const gifsOnlyOption = document.getElementById('gifs-only-option');
+const memeModalInner = document.getElementById('meme-modal-inner');
+const memeModal = document.getElementById('meme-modal');
 
 emotionRadios.addEventListener('change', highlightCheckedOption);
 
@@ -43,7 +45,16 @@ function getSingleCatObject() {
 }
 
 function renderCat() {
-    getSingleCatObject(); // temporary
+    const catObject = getSingleCatObject();
+
+    memeModalInner.innerHTML = `
+    <img 
+    class="cat-img" 
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >
+    `;
+    memeModal.style.display = 'flex';
 }
 
 function getEmotionsArray(cats) {
