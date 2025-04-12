f26334d76cb89ead72cea6bc3bb149333ee307c5
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-12 10:23:30 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Pumpkin's Purrfect Meme Picker - Aside: filter()

---
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/02. Pumpkin's Purrfect Meme Picker/08. Aside: filter()/index.js
@@ -0,0 +1,9 @@
+const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18]
+
+const adults = ages.filter(function(age){
+    return age >= 18
+})
+
+const children = ages.filter(function(age){
+    return age < 18
+})
