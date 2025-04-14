510a7449d505417fc2f0bbdad160f1e58b50306e
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-14 07:12:34 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - Aside: array.reduce()

---
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/06. Aside: array.reduce()/index.js
@@ -0,0 +1,8 @@
+const rainJanuaryByWeek = [10, 20, 0, 122];
+
+const totalRainfallJanuary = rainJanuaryByWeek.reduce(function (total, currentElement) {
+    console.log('total: ' + total, 'currentElement: ' + currentElement);
+    return total + currentElement;
+});
+
+console.log(totalRainfallJanuary);
