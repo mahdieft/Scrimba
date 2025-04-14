ccd097f5f1f953e28f370bdb86e48e47bde78130
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-14 08:29:00 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - Spread Syntax (...)

---
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/09. Spread Syntax (...)/index.js
@@ -0,0 +1,7 @@
+const lunchMenu = ['Greek Salad', 'Open Sandwich', 'Parsnip Soup', 'Flatbread and Dip'];
+const dinnerMenu = ['Lasagne', 'Strogonoff', 'Tagine', 'Katsu Curry'];
+const sweetMenu = ['Mixed Berry Ice Cream', 'Chocolate Brownie', 'Orange Cheesecake'];
+
+const eventMenu = [...lunchMenu, ...dinnerMenu, ...sweetMenu];
+
+console.log(eventMenu);
