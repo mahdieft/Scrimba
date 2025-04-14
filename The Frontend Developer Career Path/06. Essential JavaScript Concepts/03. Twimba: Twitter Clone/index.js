1613dd77d6129ce405e790fb1fbe63aeafb47981
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-13 20:32:14 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Twimba: Twitter Clone - UX fixes

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/03. Twimba: Twitter Clone/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/03. Twimba: Twitter Clone/index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from './data.js';
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-const tweetInput = document.getElementById('tweet-input');
-
 document.addEventListener('click', function (e) {
     if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like);
@@ -50,6 +48,11 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
+    const tweetInput = document.getElementById('tweet-input');
+    if (!tweetInput.value) {
+        return;
+    }
+
     tweetsData.unshift({
         handle: `@Scrimba`,
         profilePic: `images/scrimbalogo.png`,
@@ -61,6 +64,7 @@ function handleTweetBtnClick() {
         isRetweeted: false,
         uuid: uuidv4(),
     });
+    tweetInput.value = '';
     render();
 }
 
