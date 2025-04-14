ce0e5a35c60cc563e598c26c3f6d274913e9a8be
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-14 08:21:16 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - The Rest Parameter

---
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/08. The Rest Parameter/index.js
@@ -0,0 +1,6 @@
+function setPermissionLevel(permissionLevel, ...names) {
+    names.forEach((name) =>
+        console.log(`${name} now has ${permissionLevel} level access.`));
+}
+
+setPermissionLevel('admin', 'Dave', 'Sally');
