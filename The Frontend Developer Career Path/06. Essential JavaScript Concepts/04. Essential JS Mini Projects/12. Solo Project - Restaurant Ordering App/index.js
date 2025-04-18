844c76d66ee4ce4c3252eb0d5ecd9597a1eac86b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-18 14:24:20 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - Solo Project - Restaurant Ordering App - adds event listener to items

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/index.js
@@ -2,3 +2,10 @@ import { menuArray } from './data.js';
 import { display_menu } from './functions.js';
 
 display_menu(menuArray);
+
+document.getElementById('menu').addEventListener('click', e => {
+    if (e.target.id.length !== 0) {
+        const item = menuArray.filter(menu => menu.id === Number(e.target.id));
+        console.log(item);
+    }
+});
