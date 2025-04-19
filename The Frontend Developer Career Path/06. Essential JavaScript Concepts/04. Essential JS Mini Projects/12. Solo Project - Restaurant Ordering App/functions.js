649baa71a906d0cb2ea68fbf42cd5755f396b59d
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-19 06:33:38 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - Solo Project - Restaurant Ordering App - creates render_order_summary function

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/functions.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/functions.js
@@ -10,15 +10,43 @@ const build_menu_item_html = item => `
     </article>
 `;
 
-const display_menu = menuItems => {
+const display_menu = menu_items => {
     let menu_html = '';
-    menuItems.forEach(item => {
+    menu_items.forEach(item => {
         menu_html += build_menu_item_html(item);
     });
     document.getElementById('menu').innerHTML = menu_html;
 };
 
+const render_order_summary = (orders, menu_map) => {
+    const order_ids = Object.keys(orders);
+
+    let order_summary = '';
+    order_ids.forEach(order_id => {
+        order_summary += `
+            <div class="order-item">
+                <p class="order-item-detail order-item-name">${menu_map.get(order_id).name}</p>
+                <p class="order-item-detail order-item-remove">remove</p>
+                <p class="order-item-detail order-item-price">${menu_map.get(order_id).price * orders[order_id]}</p>
+            </div>
+        `;
+    });
+
+    const order_summary_div = document.getElementById('order_summary');
+    if (order_summary) {
+        order_summary_div.innerHTML = `
+            <h2>Your Order</h2>
+            ${order_summary}
+            <button class="order-total-complete">Complete order</button>
+        `;
+        order_summary_div.classList.remove('hidden');
+    } else {
+        order_summary_div.classList.add('hidden');
+    }
+};
+
+
 export {
-    build_menu_item_html,
     display_menu,
+    render_order_summary,
 };
