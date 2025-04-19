d7fdac726cdfa12163de6b99fc9bad7079617b08
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-04-19 06:33:10 -0400

    The Frontend Developer Career Path - Essential JavaScript Concepts - Essential JS Mini Projects - Solo Project - Restaurant Ordering App - creates order map for faster access time

--- The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/index.js
+++ The Frontend Developer Career Path/06. Essential JavaScript Concepts/04. Essential JS Mini Projects/12. Solo Project - Restaurant Ordering App/index.js
@@ -1,11 +1,22 @@
-import { menuArray } from './data.js';
-import { display_menu } from './functions.js';
+import { menu_array } from './data.js';
+import { display_menu, render_order_summary } from './functions.js';
 
-display_menu(menuArray);
+display_menu(menu_array);
+
+const orders = {};
+const menu_map = new Map();
+menu_array.forEach(item => {
+    menu_map.set(String(item.id), item);
+});
 
 document.getElementById('menu').addEventListener('click', e => {
     if (e.target.id.length !== 0) {
-        const item = menuArray.filter(menu => menu.id === Number(e.target.id));
-        console.log(item);
+        const item = menu_map.get(e.target.id);
+        if (item.id in orders) {
+            orders[item.id]++;
+        } else {
+            orders[item.id] = 1;
+        }
     }
+    render_order_summary(orders, menu_map);
 });
