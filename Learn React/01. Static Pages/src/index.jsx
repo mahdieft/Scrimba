c8a16d344d979e2f6ab5974c3c2f8337cc1c48a6
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-19 23:44:09 -0400

    Learn React - Static Pages - Styling with Classes

--- Learn React/01. Static Pages/src/index.jsx
+++ Learn React/01. Static Pages/src/index.jsx
@@ -4,8 +4,15 @@ const root = createRoot(document.getElementById('root'));
 
 function Header() {
     return (
-        <header>
-            <img src="/src/images/react-logo.png" width="40px" alt="React logo" />
+        <header className="header">
+            <img src="/src/images/react-logo.png" className="nav-logo" alt="React logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
         </header>
     );
 }
