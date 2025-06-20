0885ba951de147e58aaad976b99488206cf02822
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-19 23:51:01 -0400

    Learn React - Static Pages - Organizing Components

---
+++ Learn React/01. Static Pages/src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>&copy;2025 Mahdi development. All rights reserved.</small>
+        </footer>
+    );
+}
--- Learn React/01. Static Pages/src/index.jsx
+++ Learn React/01. Static Pages/src/index.jsx
@@ -1,41 +1,9 @@
 import { createRoot } from 'react-dom/client';
+import Header from './Header.jsx';
+import MainContent from './MainContent.jsx';
+import Footer from './Footer.jsx';
 
 const root = createRoot(document.getElementById('root'));
-
-function Header() {
-    return (
-        <header className="header">
-            <img src="/src/images/react-logo.png" className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
-
-function MainContent() {
-    return (
-        <main>
-            <h1>Reason I am excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front-end developer if I know React</li>
-            </ol>
-        </main>
-    );
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>&copy;2025 Mahdi development. All rights reserved.</small>
-        </footer>
-    );
-}
 
 function Page() {
     return (
